8bdff72cd300238941898a03dae9576d0ac19734
Harsh Yadav <glorytoharsh@gmail.com>
2022-01-22 12:13:14 +0000

    feat: add test and complete contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,17 +14,19 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const DumbGuys = await hre.ethers.getContractFactory("DumbGuys");
+  const dumbGuys = await DumbGuys.deploy();
 
-  await greeter.deployed();
+  await dumbGuys.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("DumbGuys deployed to:", dumbGuys.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main().catch((error) => {
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
   console.error(error);
   process.exitCode = 1;
 });
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,24 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+describe("nft", function () {
+  it("Should mint and transfer NFT to buyer", async function () {
+    const DumbGuys = await ethers.getContractFactory("DumbGuys");
+    const dumbGuys = await DumbGuys.deploy();
+    await dumbGuys.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    const buyer = '0x8626f6940e2eb28930efb4cef49b2d1f2c9c1199';
+    const metadataURI = 'cid/test.png';
+    
+    let balance = await dumbGuys.balanceOf(buyer);
+    expect(balance).to.equal(0);
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const newlyMintedToken = await dumbGuys.payToMint(buyer, metadataURI, { value: ethers.utils.parseEther('0.01') })
+  
+    await newlyMintedToken.wait();
+    balance = await dumbGuys.balanceOf(buyer);
+    expect(balance).to.equal(1);
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+    expect(await dumbGuys.isContentOwned(metadataURI)).to.equal(true);
   });
 });
